9d04fcd046a9635e706254f847daffb69f89542c
kellyjeffery <kellyjeffery1@hotmail.com>
2020-02-06 16:52:18 +0000

    delet button

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    count: this.props.value
   };
 
   hanldeIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.count + 1 });
+    this.setState({ count: this.state.count + 1 });
   };
 
   doHandleIncrement = () => {
@@ -24,6 +23,13 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
